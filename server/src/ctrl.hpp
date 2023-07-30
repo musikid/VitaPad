62c6f70a6b84e9e122da6a8db7b7ce419d99c29a
Sayafdine Said <musikid@outlook.com>
2023-07-30 14:04:27 +0200

    fix: enable flatbuffers verifier

--- server/src/ctrl.hpp
+++ server/src/ctrl.hpp
@@ -1,6 +1,7 @@
 #ifndef __CTRL_H__
 #define __CTRL_H__
 
+#define FLATBUFFERS_TRACK_VERIFIER_BUFFER_SIZE
 #include <netprotocol_generated.h>
 
 flatbuffers::FlatBufferBuilder get_ctrl_as_netprotocol();
